8d382e2775d117fd6663149e6ebe969e90c21be8
Robert James <robert.hugemail@gmail.com>
2017-01-05 15:51:35 +0000

    Added script to get ip and hostname then merge them together and add them into /etc/hosts

---
+++ getip-and-host-add-to-hosts/getip-and-host-add-to-hosts.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+MYIP=$(ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
+MYHOSTNAME=$(hostname)
+echo $MYIP
+echo $MYHOSTNAME
+RESULT=`echo -e $MYIP"\t"$MYHOSTNAME`
+echo "$RESULT"
